6ac6c319fa4b8fc46b93ec297539f25f1a3997de
yburkul <yburkul123@gmail.com>
2022-03-07 14:38:51 +0530

    Added:switch case

--- Program.cs
+++ Program.cs
@@ -1,32 +1,39 @@
-﻿Console.WriteLine("Welcome to Employee Wage Computation Program");
+﻿using System;
 
-//UC3
-int Present = 1; //Constant
-int Fulltime = 1;
-int Emp_per_rate = 20;
-int EmpHr = 0;
-int Emp_wage = 0;
-Random random = new Random(); //Computation // to create object
-Random random1 = new Random(); // creating second object
-int EmpStatus = random.Next(0, 2);
-int Emp_Time = random1.Next(0, 2);
-if (EmpStatus == Present)
+namespace EmpWage
 {
-    if (Emp_Time == Fulltime)
+    class Program
     {
-        Console.WriteLine("Employee is Present");
-        EmpHr = 8;
-    }
-    else
-    {
-        Console.WriteLine("Employee is Present");
-        EmpHr = 4;
+        //UC4
+        public const int is_part_time = 1;
+        public const int is_full_time = 2;
+        public const int emp_hr_per_rate = 20;
+
+        static void Main(string[] args)
+        {
+            int empHr = 0;
+            int empwage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case is_part_time:
+                    empHr = 4;
+                    Console.WriteLine("Employee is Present");
+                    Console.WriteLine("Employee is Part time");
+                    break;
+                case is_full_time:
+                    empHr = 8;
+                    Console.WriteLine("Employee is Present");
+                    Console.WriteLine("Employee is Full time");
+                    break;
+                default:
+                    empHr = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
+            }
+            empwage = (emp_hr_per_rate * empHr);
+            Console.WriteLine("Daily emp wage: " + empwage);
+        }
     }
 }
-else
-{
-    Console.WriteLine("Employee is Absent");
-    EmpHr = 0;
-}
-Emp_wage = (Emp_per_rate * EmpHr);
-Console.WriteLine("Daily wage " + Emp_wage);
