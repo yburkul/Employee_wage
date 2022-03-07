cbc0be9e4e08e48bd2c9a663a3c37028bf0e0f96
yburkul <yburkul123@gmail.com>
2022-03-07 14:35:38 +0530

    Added:employee wage

--- Program.cs
+++ Program.cs
@@ -1,16 +1,27 @@
 ﻿Console.WriteLine("Welcome to Employee Wage Computation Program");
 
-//UC2
+//UC3
 int Present = 1; //Constant
+int Fulltime = 1;
 int Emp_per_rate = 20;
 int EmpHr = 0;
 int Emp_wage = 0;
-Random random = new Random(); //Computation
+Random random = new Random(); //Computation // to create object
+Random random1 = new Random(); // creating second object
 int EmpStatus = random.Next(0, 2);
+int Emp_Time = random1.Next(0, 2);
 if (EmpStatus == Present)
 {
-    Console.WriteLine("Employee is Present");
-    EmpHr = 8;
+    if (Emp_Time == Fulltime)
+    {
+        Console.WriteLine("Employee is Present");
+        EmpHr = 8;
+    }
+    else
+    {
+        Console.WriteLine("Employee is Present");
+        EmpHr = 4;
+    }
 }
 else
 {
