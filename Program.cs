14e421d9f184519a63b9face96bbae009471094f
yburkul <yburkul123@gmail.com>
2022-03-07 14:26:46 +0530

    Added:Attendance

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
 ﻿Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+//UC1
+int Present = 1; //Constant
+Random random = new Random(); //Computation
+int EmpStatus = random.Next(0, 2);
+if (EmpStatus == Present)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+}
